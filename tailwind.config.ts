8a19ba58f8c3477b2c16030af936551ec8fa810f
Pridestalkerr <danartenie158@gmail.com>
2023-05-01 07:41:26 +0300

    added tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,25 @@
 import { type Config } from "tailwindcss";
 
+const colors = {
+  darker: "#393646",
+  dark: "#4F4557",
+  light: "#6D5D6E",
+  lighter: "#F4EEE0",
+} as const;
+
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: colors,
+      textColor: colors,
+      borderColor: colors,
+      hover: {
+        backgroundColor: colors,
+        textColor: colors,
+        borderColor: colors,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
